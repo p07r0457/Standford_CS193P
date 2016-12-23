1937959cacd94fc352250138af3498043206f941
Kyle Maher <p07r0457@gmail.com>
2016-12-22 23:49:45 -0800

    Initial commit.  Empty project.

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kyle Maher on 12/22/16.
+//  Copyright © 2016 Kyle Maher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
